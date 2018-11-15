484c1b8b881940d61b4682a8d1a5567b2834da6d
Морозов Юрий <moroz69off@outlook.com>
2018-11-15 22:55:28 +0300

    2-grama OK

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TextAnalysis
 {
@@ -6,9 +7,28 @@ namespace TextAnalysis
     {
         public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
         {
-            var result = new Dictionary<string, string>();
-            //...
-            return result;
+            Dictionary<string, string> n_gramDict = new Dictionary<string, string>();
+            List<string> x2grams = new List<string>();
+            if (text.Count > 0)
+            {
+                for (int i = 0; i < text.Count; i++)
+                {
+                    List<string> x2gramsTemp = Get2grams(text[i]);
+                    x2grams.AddRange(x2gramsTemp);
+                }
+                x2grams.Sort();
+            }
+            return n_gramDict;
         }
-   }
+
+        private static List<string> Get2grams(List<string> list)
+        {
+            List<string> x2gramsTemp = new List<string>();
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                x2gramsTemp.Add(list[i] + " " + list[i + 1]);
+            }
+            return x2gramsTemp;
+        }
+    }
 }
