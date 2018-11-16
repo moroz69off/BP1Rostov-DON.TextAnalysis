65935c16d69e8c02800fdd87622bff05321d84d9
Морозов Юрий <moroz69off@outlook.com>
2018-11-16 14:34:35 +0300

    clearNgram

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -9,22 +9,50 @@ namespace TextAnalysis
         {
             Dictionary<string, string> result = new Dictionary<string, string>();
             List<KeyValuePair<string, string>> listKeyValuePair = new List<KeyValuePair<string, string>>();
-            List<string> x2grams = new List<string>();
+            List<string> nGramas = new List<string>();
             if (text.Count > 0)
             {
                 for (int i = 0; i < text.Count; i++)
                 {
-                    List<string> x2gramsTemp = Get2grams(text[i]);
-                    x2grams.AddRange(x2gramsTemp);
+                    List<string> n2gramasTemp = Get2gramas(text[i]);
+                    nGramas.AddRange(n2gramasTemp);
+                    List<string> n3gramasTemp = Get3gramas(text[i]);
+                    nGramas.AddRange(n3gramasTemp);
                 }
-                x2grams.Sort();
-                for (int i = 0; i < x2grams.Count; i++)
-                    listKeyValuePair.Add(new KeyValuePair<string, string>(x2grams[i].Split()[0], x2grams[i].Split()[1]));
+                nGramas.Sort();
+                for (int i = 0; i < nGramas.Count; i++)
+                {
+                    if (nGramas[i].Split().Length == 2)
+                    {
+                        listKeyValuePair.Add(new KeyValuePair<string, string>(nGramas[i].Split()[0], nGramas[i].Split()[1]));
+                    }
+                    else 
+                    if (nGramas[i].Split().Length == 3)
+                    {
+                        listKeyValuePair.Add(new KeyValuePair<string, string>(
+                            nGramas[i].Split()[0], 
+                            nGramas[i].Split()[1]));
+                        listKeyValuePair.Add(new KeyValuePair<string, string>(
+                            nGramas[i].Split()[0] + 
+                            " " + nGramas[i].Split()[1], 
+                            nGramas[i].Split()[2]));
+                    }
+                }
             }
             return result;
         }
 
-        private static List<string> Get2grams(List<string> list)
+        private static List<string> Get3gramas(List<string> list)
+        {
+            List<string> x2gramsTemp = new List<string>();
+            for (int i = 0; i < list.Count - 2; i++)
+            {
+                x2gramsTemp.Add(list[i] + " " + list[i + 1] + " " + list[i + 2]);
+            }
+            return x2gramsTemp;
+        }
+
+        private static List<string> Get2gramas(List<string> list)
         {
             List<string> x2gramsTemp = new List<string>();
             for (int i = 0; i < list.Count - 1; i++)
