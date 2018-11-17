48f5ca4d0eceb4928653fdac08ea1d0bfcdd24fe
Морозов Юрий <moroz69off@outlook.com>
2018-11-17 14:05:44 +0300

    evrica

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -8,58 +8,66 @@ namespace TextAnalysis
         public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
         {
             Dictionary<string, string> result = new Dictionary<string, string>();
-            List<KeyValuePair<string, string>> listKeyValuePair = new List<KeyValuePair<string, string>>();
-            List<string> nGramas = new List<string>();
-            if (text.Count > 0)
+            Dictionary<string, int> frequence = new Dictionary<string, int>();
+
+            List<string> allx2gramas = new List<string>(Get2gramas(text));
+            
+            foreach (var item in allx2gramas)
             {
-                for (int i = 0; i < text.Count; i++)
+                if (!frequence.ContainsKey(item))
+                    frequence.Add(item, 1);
+                else
+                    frequence[item]++;
+            }
+
+            List<string> allx3gramas = new List<string>(Get3gramas(text));
+
+            foreach (var item in allx3gramas)
+            {
+                if (!frequence.ContainsKey(item))
+                    frequence.Add(item, 1);
+                else
+                    frequence[item]++;
+            }
+
+            List<string> keys = new List<string>(frequence.Keys);
+
+            for (int i = 0; i < keys.Count - 1; i++)
+            {
+                if (
+                    true // Условие для пары key-value: одинаковые начала есть но концов больше. Или одинаковые начала есть и концов динаково но конец старше. Или таких начал больше нет.
+                    )
                 {
-                    List<string> n2gramasTemp = Get2gramas(text[i]);
-                    nGramas.AddRange(n2gramasTemp);
-                    List<string> n3gramasTemp = Get3gramas(text[i]);
-                    nGramas.AddRange(n3gramasTemp);
-                }
-                nGramas.Sort();
-                for (int i = 0; i < nGramas.Count; i++)
-                {
-                    if (nGramas[i].Split().Length == 2)
-                    {
-                        listKeyValuePair.Add(new KeyValuePair<string, string>(nGramas[i].Split()[0], nGramas[i].Split()[1]));
-                    }
-                    else 
-                    if (nGramas[i].Split().Length == 3)
-                    {
-                        listKeyValuePair.Add(new KeyValuePair<string, string>(
-                            nGramas[i].Split()[0], 
-                            nGramas[i].Split()[1]));
-                        listKeyValuePair.Add(new KeyValuePair<string, string>(
-                            nGramas[i].Split()[0] + 
-                            " " + nGramas[i].Split()[1], 
-                            nGramas[i].Split()[2]));
-                    }
+                    // и начало и конец отправляется в ответ
                 }
             }
             return result;
         }
 
-        private static List<string> Get3gramas(List<string> list)
+        private static List<string> Get3gramas(List<List<string>> text)
         {
-            List<string> x2gramsTemp = new List<string>();
-            for (int i = 0; i < list.Count - 2; i++)
+            List<string> res = new List<string>();
+            foreach (var item in text)
             {
-                x2gramsTemp.Add(list[i] + " " + list[i + 1] + " " + list[i + 2]);
+                for (int i = 0; i < item.Count - 2; i++)
+                    res.Add(item[i] + " " 
+                        + item[i + 1] + " " 
+                        + item[i + 2]);
             }
-            return x2gramsTemp;
+            res.Sort();
+            return res;
         }
 
-        private static List<string> Get2gramas(List<string> list)
+        private static List<string> Get2gramas(List<List<string>> text)
         {
-            List<string> x2gramsTemp = new List<string>();
-            for (int i = 0; i < list.Count - 1; i++)
+            List<string> res = new List<string>();
+            foreach (var item in text)
             {
-                x2gramsTemp.Add(list[i] + " " + list[i + 1]);
+                for (int i = 0; i < item.Count - 1; i++)
+                    res.Add(item[i] + " " + item[i + 1]);
             }
-            return x2gramsTemp;
+            res.Sort();
+            return res;
         }
     }
 }
