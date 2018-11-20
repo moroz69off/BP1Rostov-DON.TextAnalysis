7088a7b2fa0df1e5f206cc31982f629b486c79c6
Морозов Юрий <moroz69off@outlook.com>
2018-11-20 04:18:41 +0300

    iterationBadly

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -5,6 +5,8 @@ namespace TextAnalysis
 {
     static class FrequencyAnalysisTask
     {
+        private static bool mue;
+
         public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
         {
             Dictionary<string, string> result = new Dictionary<string, string>();
@@ -32,18 +34,56 @@ namespace TextAnalysis
 
             List<string> keys = new List<string>(frequence.Keys);
 
-            for (int i = 0; i < keys.Count - 1; i++)
+            List<KeyValuePair<string, string>> keyValuePairs = new List<KeyValuePair<string, string>>();
+
+            for (int i = 0; i < keys.Count; i++)
             {
-                if (
-                    true // Условие для пары key-value: одинаковые начала есть но концов больше. Или одинаковые начала есть и концов динаково но конец старше. Или таких начал больше нет.
-                    )
+                keyValuePairs.Add(new KeyValuePair<string, string>(keys[i].Substring(0, keys[i].LastIndexOf(' ')), 
+                    keys[i].Substring(keys[i].LastIndexOf(' '))));
+                if (Mue(i, keys, frequence, keyValuePairs))
                 {
-                    // и начало и конец отправляется в ответ
+                    //пара отправляется в ответ
                 }
             }
             return result;
         }
 
+        private static bool Mue(int i,
+            List<string>keys, 
+            Dictionary<string, int> frequence, 
+            List<KeyValuePair<string, string>> kvp)
+        {// Условие для пары key-value: 
+         // Одинаковые начала есть, но этих концов больше. 
+         // Или: одинаковые начала есть, и концов одинаково, но конец меньше в строю. 
+         // Или: таких начал больше нет.
+
+            string keyCondidat = keys[i];
+            int fr = frequence[keyCondidat];
+            bool hiFreq = CompareFreq(i,keys,frequence,kvp);
+            if (!
+                (i==0)
+                )
+                return true;
+            else  return false; 
+        }
+
+        private static bool CompareFreq(int i, 
+            List<string> keys, 
+            Dictionary<string, int> frequence, 
+            List<KeyValuePair<string, string>> kvp)
+        {
+            string start = keys[i].Split()[0];
+            List<string> list = new List<string>();
+            for (int j = 0; j < keys.Count; j++)
+                if (keys[j].Split()[0] == keys[i].Split()[0])
+                    list.Add(keys[j]);
+            for (int j = 0; j < list.Count; j++)
+                if (frequence[keys[i]] > frequence[list[j]])
+                    list.Remove(keys[i]); // список одинаковых началов для i-того члена
+            return true;
+        }
+
+        //=====================================================================
         private static List<string> Get3gramas(List<List<string>> text)
         {
             List<string> res = new List<string>();
@@ -57,17 +97,19 @@ namespace TextAnalysis
             res.Sort();
             return res;
         }
-
+        //---------------------------------------------------------------------wcido?
         private static List<string> Get2gramas(List<List<string>> text)
         {
             List<string> res = new List<string>();
             foreach (var item in text)
             {
                 for (int i = 0; i < item.Count - 1; i++)
-                    res.Add(item[i] + " " + item[i + 1]);
+                    res.Add(item[i] + " " 
+                        + item[i + 1]);
             }
             res.Sort();
             return res;
         }
+        //=====================================================================
     }
 }
--- SentencesParserTask.cs
+++ SentencesParserTask.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text;
+
 namespace TextAnalysis
 {
     static class SentencesParserTask
@@ -7,10 +9,9 @@ namespace TextAnalysis
         public static List<List<string>> ParseSentences(string text)
         {
             char[] sentenceSeparators = new char[]{'.', '!', '?', ';', ':', '(', ')' };
-            text = text.Normalize();
             string[] sentences = text
                 .Split(sentenceSeparators,
-                System.StringSplitOptions.RemoveEmptyEntries);
+                StringSplitOptions.RemoveEmptyEntries);
             return ParseAndAddWordsToLists(sentences);
         }
 
@@ -20,24 +21,40 @@ namespace TextAnalysis
             char[] wordSeparators = new char[] { ' ', ',' };
             for (int i = 0; i < sentences.Length; i++)
             {
-                StringBuilder sb = new StringBuilder();
-                char[] chars = sentences[i].ToLower().ToCharArray();
-                for (int l = 0; l < chars.Length; l++)
-                {
-                    if (char.IsLetter(chars[l]) || chars[l] == '\'')
-                        sb.Append(chars[l]);
-                    else
-                        sb.Append(' ');
-                }
-                sentences[i] = sb.ToString().Trim(wordSeparators);
-                List<string> words = new List<string>(sentences[i]
-                    .Split(
-                    wordSeparators,
-                    System.StringSplitOptions.RemoveEmptyEntries));
+                List<string> words = SentencesParse(sentences, wordSeparators, i);
                 sentencesList.Add(words);
             }
-            sentencesList.RemoveAll(x => x.Count == 0);
-            return sentencesList;
+            return RemoteEmptyWords(sentencesList);
+        }
+
+        private static List<List<string>> RemoteEmptyWords(List<List<string>> sentencesList)
+        {
+            List<List<string>> list = new List<List<string>>(sentencesList.Count / 8);
+            foreach (var item in sentencesList)
+                if (item.Count != 0)
+                    list.Add(item);
+            return list;
+        }
+
+        private static List<string> SentencesParse(string[] sentences, 
+            char[] wordSeparators, int i)
+        {
+            StringBuilder sb = new StringBuilder();
+            char[] chars = sentences[i].ToLower().ToCharArray();
+            for (int l = 0; l < chars.Length; l++)
+            {
+                if (char.IsLetter(chars[l]) || chars[l] == '\'')
+                    sb.Append(chars[l]);
+                else
+                    sb.Append(' ');
+            }
+            sentences[i] = sb.ToString().Trim(wordSeparators);
+            List<string> words = new List<string>(sentences[i]
+                .Split(
+                wordSeparators,
+                StringSplitOptions.RemoveEmptyEntries));
+            words.RemoveAll(x => x.Length == 0);
+            return words;
         }
     }
 }
