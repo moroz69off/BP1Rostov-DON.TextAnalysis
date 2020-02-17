12e020b94c05854f86d16c8eb6c5e11b08392d2c
moroz69off.yuri <moroz69off@outlook.com>
2020-02-17 20:44:31 +0300

    Ещё один подход к решению задания

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -1,24 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-// Условие для пары key-value: 
-// Одинаковые начала есть, но этих концов больше. 
-// Или: одинаковые начала есть, и концов одинаково, но конец меньше в строю. 
-// Или: таких начал больше нет.
+
 namespace TextAnalysis
 {
     public class Frequency
     {
         public string start, and;
         public int count;
+        public bool isResult;
 
         public Frequency(string start, string and, int count)
         {
             this.start = start;
             this.and = and;
             this.count = count;
-        }
+        }// Не знаю, нужен ли этот конструктор?
 
-        public KeyValuePair<string, string> FindFrequencyElement(string start)
+        public KeyValuePair<string, string> FrequencyElement(string start)
         {
             KeyValuePair<string, string> kvp = new KeyValuePair<string, string>();
             return kvp;
@@ -29,8 +27,57 @@ namespace TextAnalysis
     {
         public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
         {
-            Dictionary<string, string> frequency = new Dictionary<string, string>();
-            return frequency;
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            Dictionary<string, int> frequency = new Dictionary<string, int>();
+            List<string> allx2gramas = new List<string>(Get2gramas(text));
+            foreach (var item in allx2gramas)
+            {
+                if (!frequency.ContainsKey(item))
+                    frequency.Add(item, 1);
+                else
+                    frequency[item]++;
+            }
+            List<string> allx3gramas = new List<string>(Get3gramas(text));
+            foreach (var item in allx3gramas)
+            {
+                if (!frequency.ContainsKey(item))
+                    frequency.Add(item, 1);
+                else
+                    frequency[item]++;
+            }
+            for (int i = 0; i < frequency.Count; i++)
+            {
+
+            }
+
+            return result;
+        }
+
+        private static List<string> Get3gramas(List<List<string>> text)
+        {
+            List<string> result = new List<string>();
+            foreach (var item in text)
+            {
+                for (int i = 0; i < item.Count - 2; i++)
+                    result.Add(item[i] + " "
+                        + item[i + 1] + " "
+                        + item[i + 2]);
+            }
+            result.Sort();
+            return result;
+        }
+
+        private static List<string> Get2gramas(List<List<string>> text)
+        {
+            List<string> result = new List<string>();
+            foreach (var item in text)
+            {
+                for (int i = 0; i < item.Count - 1; i++)
+                    result.Add(item[i] + " "
+                        + item[i + 1]);
+            }
+            result.Sort();
+            return result;
         }
     }
 }
